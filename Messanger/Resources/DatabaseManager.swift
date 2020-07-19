1106a542ab87b0999df11c2574f511c5d609eb0e
Md Maruf Howlader <maruf.dhaka2010@gmail.com>
2020-07-19 19:17:49 +0600

    end of video 5 successfully This app new can create an entry into our database with authentication entry

--- Messanger/Resources/DatabaseManager.swift
+++ Messanger/Resources/DatabaseManager.swift
@@ -14,28 +14,18 @@ class DatabaseManger{
     static let shared = DatabaseManger()
     private var database = Database.database().reference()
     
-    public func insertUser(with user: ChatAppUser){
-        database.child(user.emailAddress).setValue([
-            "firstname": user.firstName,
-            "lastname": user.lastName,
-            "emailaddress": user.emailAddress,
-            "password": user.password
-        ])
-    }
-    public struct ChatAppUser{
-         let firstName: String
-         let lastName: String
-         let emailAddress: String
-         let password: String
-//        let profilePictureURL: String
-    }
+   
 //    public func test(){
 //        database.child("uniqueId2").setValue(["Name":"Opu", "Age":29])
 //    }
 }
 extension DatabaseManger{
     public func userExist(with email: String, completion: @escaping ((Bool) -> Void)){
-        database.child(email).observeSingleEvent(of: .value) { (snapshoot) in
+        
+        var safeEmail = email.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        
+        database.child(safeEmail).observeSingleEvent(of: .value) { (snapshoot) in
             guard snapshoot.value as? String != nil else{
                 completion(false)
                 return
@@ -44,4 +34,27 @@ extension DatabaseManger{
             completion(true)
         }
     }
+    
+     public func insertUser(with user: ChatAppUser){
+            database.child(user.safeEmail).setValue([
+                "firstname": user.firstName,
+                "lastname": user.lastName,
+                //"emailaddress": user.safeEmail,
+                "password": user.password
+            ])
+        }
+        public struct ChatAppUser{
+             let firstName: String
+             let lastName: String
+             let emailAddress: String
+             let password: String
+    //        let profilePictureURL: String
+           
+            var  safeEmail: String {
+                var safeEmail = emailAddress.replacingOccurrences(of: ".", with: "-")
+                safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+                return safeEmail
+            }
+        }
+    
 }
